46b490b31a151e7d7fe53e397b7b0428489ad0fd
ITSEEFAT <149010648+ITSEEFAT@users.noreply.github.com>
2024-04-04 11:33:01 +0000

    container config done

---
+++ 01_basic/test.js
@@ -0,0 +1,8 @@
+// why we use let in js ?? why not var
+// bcs scope of variable 
+const id="38948"
+let name="syed"
+let roll="478340"
+roll="2201921630057"
+password="387rikj"
+console.table([id,name,roll,password])
