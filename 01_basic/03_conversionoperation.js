9aec5566e7e1e0de2b38eb3120a9154b63515abd
ITSEEFAT <149010648+ITSEEFAT@users.noreply.github.com>
2024-04-09 18:47:11 +0000

    container config done

---
+++ 01_basic/03_conversionoperation.js
@@ -0,0 +1,28 @@
+// code conversion from string to number//
+  let a="23" //this is string
+  let num = Number(a) // this is now number
+
+ console.log(typeof a)
+console.log(typeof(num))
+console.log(num)
+// string containing number only  can be converted to  number ... but when other key words then it will print "NAN"
+
+
+//ex
+let b="d4334"
+let numb= Number(b)
+console.log(typeof numb)
+console.log(numb)
+
+// Boolean 0-false 1-true or true:non empty  false:empty
+
+let c=1
+let ex=Boolean(c)
+console.log(ex)
+
+
+let d=""
+let ed= Boolean(d)
+// d is empty therefore false
+console.log(ed)
+
