1909434ddebb0477893074321f5e87da3aa06dec
ITSEEFAT <149010648+ITSEEFAT@users.noreply.github.com>
2024-04-14 19:10:31 +0000

    config done

---
+++ 01_basic/04_typesOfdataTypes.js
@@ -0,0 +1,35 @@
+// we have 2 types of data types
+
+// 1.primitive 
+// 2.non primitive (reference)
+
+
+// primitive have 7 types which include string number boolean undefined Bigint symboll null
+
+
+// non primitive 
+// array object function4
+ 
+ const arrayname=["rahul", 34,"cricket"]
+
+
+//  creating object
+let objectname={
+
+  name :"rahul",
+  roll:43,
+  class:10,
+
+
+
+}
+
+
+// /creating function
+const functionName=function(){
+
+    console.log("hello world")
+}
+
+
+
